5749c69d9819eb600b12afa2fed778224f4d49f6
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-30 20:02:13 -0300

    feat: implementando funcionalidade  nas tarefas pra alterar estaod

--- src/app/components/Tarefa/index.tsx
+++ src/app/components/Tarefa/index.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image';
 import './style.css';
 
-export const Tarefa = ({ tarefa, status, openModalDelete }: { tarefa: string, status: boolean, openModalDelete: () => void }) => {
-    const handleTrashClick = () => {
-      openModalDelete();
-    };
+export const Tarefa = ({ id, tarefa, status, openModalDelete, onToggleStatus }: { id: number, tarefa: string, status: boolean, openModalDelete: () => void, onToggleStatus: () => void }) => {
+  const handleTrashClick = () => {
+    openModalDelete();
+  };
+  const handleCheckboxChange = () => {
+    onToggleStatus();
+  };
 
-    return (
-        <div className='product'>
-          <div>
-            <input type="checkbox" name="to-do" />
-            {status && <span>{tarefa}</span>}
-            {!status && <span className='tarefa-concluida'>{tarefa}</span>}
-          </div>
-          <Image alt='trash' src={'trash.svg'} height={24} width={24} className='trash' onClick={handleTrashClick} />
-        </div>
-      );
+
+  return (
+    <div className='product'>
+      <div>
+        <input type="checkbox" name="to-do" checked={!status} onChange={handleCheckboxChange} />
+        {status && <span>{tarefa}</span>}
+        {!status && <span className='tarefa-concluida'>{tarefa}</span>}
+      </div>
+      <Image alt='trash' src={'trash.svg'} height={24} width={24} className='trash' onClick={handleTrashClick} />
+    </div>
+  );
 };
