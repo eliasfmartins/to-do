7029eca634e5003a9e4ed384f064be6dff779097
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-31 11:08:58 -0300

    feat: att name

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import './style.css'
-export const Header =()=>{
+export const Header = () => {
 
-    return(
+    return (
         <header>
-                <a href=""><Image alt='logo' src={"logo.svg"} width={180} height={40}/></a>
-                <a href="">
-                    <h2 className='wellcome'>
-                    Bem-vindo de volta, Marcus
-                    </h2>
-                    </a>
-                <p className='data'>Segunda,22 de dezembro de 2023</p>
+            <a href=""><Image alt='logo' src={"logo.svg"} width={180} height={40} /></a>
+            <a href="">
+                <h2 className='wellcome'>
+                    Bem-vindo de volta, meu Consagrado {' "----"'}
+                </h2>
+            </a>
+            <p className='data'>Segunda,22 de dezembro de 2023</p>
         </header>
     )
 }
